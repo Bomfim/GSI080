bba19fbc56e1ac01cfc33f8148d14f68a02e5338
Bomfim <mariocesarbomfim@gmail.com>
2017-11-06 21:32:09 -0200

    In Progress...

--- UMA/test.c
+++ UMA/test.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
 #include "uailloc.c"
+
 int main()
 {
+<<<<<<< HEAD
 	int *ptr;
 	for (int i = 0; i < (2 ^ 8); i++)
 	{
@@ -9,5 +10,20 @@ int main()
 		printf("%p\n",&ptr);
 		free(ptr);
 	}
+=======
+	char *ptr,*ptr2,*ptr3;
+	ptr = (char*) malloc(81920);
+	ptr2 = (char*) malloc(40960);
+	free(ptr);
+	ptr3 = (char *) malloc(81920);
+	
+	
+	fprintf(stderr, "free: %p\n", ptr);
+	free(ptr2);
+	fprintf(stderr, "free: %p\n", ptr2);
+	free(ptr3);
+	fprintf(stderr, "free: %p\n", ptr3);
+	
+>>>>>>> b35532aed529c3717ea935ae0f2b6582191e68cc
 	return 0;
 }
--- UMA/uailloc.c
+++ UMA/uailloc.c
@@ -1,20 +1,18 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
 
+//Struct para representar o bloco + header.
 typedef struct header_t {
 	size_t size;
 	unsigned is_free;
 	struct header_t *next;
 } Header;
 
-
+//Cabeça e calda da lista ligada.
 Header *head, *tail;
 
-pthread_mutex_t global_malloc_lock;
-
-
+//Retorna o 1º bloco livre (first-fit).
 Header *get_free_block(size_t size)
 {
 	Header *current = head;
@@ -32,22 +30,20 @@ void *malloc(size_t size)
 	void *block;
 	Header *header;
 
-	if (!size)
+	if (!size)		// Se o tamanho for inválido.
 		return NULL;
-	pthread_mutex_lock(&global_malloc_lock);
+	
 	header = get_free_block(size);
 
-	if (header) {
+	if (header) {	// Se ele possui um bloco do tamanho desejado.
 		header->is_free = 0;
-		pthread_mutex_unlock(&global_malloc_lock);
 		return (void*)(header + 1);
 	}
 
 	total_size = sizeof(Header) + size;
-	block = sbrk(total_size);
+	block = sbrk(total_size);		// Caso contrário, aloca-se via syscall um novo pedaço de memória.
 
-	if (block == (void*) -1) {
-		pthread_mutex_unlock(&global_malloc_lock);
+	if (block == (void*) -1) { // Teste de retorno da sbrk.
 		return NULL;
 	}
 
@@ -55,49 +51,46 @@ void *malloc(size_t size)
 	header->size = size;
 	header->is_free = 0;
 	header->next = NULL;
-	
-	printf("%p\n", header);
 
-	if (!head)
+	fprintf(stderr, "%p\t%d\t%d\t%p\n",header, header->size, header->is_free, header->next);
+	
+	if (!head)		// Teste se for o primeiro a ser inserido, atualiza a cabeça.
 		head = header;
-	if (tail)
+	if (tail)		// Teste se for o primeiro a ser inserido, atualiza a calda.
 		tail->next = header;
 	tail = header;
-	pthread_mutex_unlock(&global_malloc_lock);
 
-	return (void*)(header + 1);
+	return (void*)(header + 1); // Retorna um ponteiro do próximo espaço requisitado.
 }
 
 
 
 void free(void *block)
 {
-	Header *header, *tmp;
+	Header *header, *aux;
 	void *programbreak;
 
-	if (!block)
+	if (!block)		// Teste valor válido
 		return;
-	pthread_mutex_lock(&global_malloc_lock);
-	header = (Header*)block - 1;
+	
+	header = (Header*)block - 1; // Pega o bloco anterior.
 
-	programbreak = sbrk(0);
-	if ((char*)block + header->size == programbreak) {
+	programbreak = sbrk(0); // Final da heap.
+	if ((char*)block + header->size == programbreak) { // Teste se o último bloco é o final da heap.
 		if (head == tail) {
-			head = tail = NULL;
+			head = tail = NULL;		// Atualiza a cabeça.
 		} else {
-			tmp = head;
-			while (tmp) {
-				if(tmp->next == tail) {
-					tmp->next = NULL;
-					tail = tmp;
+			aux = head;
+			while (aux) {			// Atualização da cauda.
+				if(aux->next == tail) {
+					aux->next = NULL;
+					tail = aux;
 				}
-				tmp = tmp->next;
+				aux = aux->next;
 			}
 		}
-		sbrk(0 - sizeof(Header) - header->size);
-		pthread_mutex_unlock(&global_malloc_lock);
+		sbrk(0 - sizeof(Header) - header->size); // Desalocando o tamanho necessário.
 		return;
 	}
-	header->is_free = 1;
-	pthread_mutex_unlock(&global_malloc_lock);
+	header->is_free = 1; // Flag de livre/usado.
 }
